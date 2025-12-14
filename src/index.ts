cb7df5f4f71b3a9ea7ea0691b015315e59f3308d
András Handler <handlerandras@gmail.com>
2025-12-14 14:03:26 +0100

    Add CORS parameters and angular port

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
